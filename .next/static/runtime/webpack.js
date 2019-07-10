d236546e4abe40aa19ef19f70b7330ad65b64131
Alex <webprom@pm.me>
2019-07-10 08:56:05 +0700

    Updaet

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6a4c157bb49ce7b38ebc";
+/******/ 	var hotCurrentHash = "1302a905b091b9ef14d3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
