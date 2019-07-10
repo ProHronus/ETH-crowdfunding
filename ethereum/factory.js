4279b8819c6a5df86114ee96d880bdc0d4fbed1b
Alex <webprom@pm.me>
2019-07-10 09:26:11 +0700

    Update

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,13 +1,13 @@
 import web3JS from './web3';
+import FactoryJSON from './build/CampaignFactory.json'
 
 
-const CampaignFactory = require('../Ethereum/build/CampaignFactory.json');
 const address = '0x827077c1EFE0eE037c2bE38f2Fe354824a3B43d7';
 
 async function factoryInit() {
 
         const web3Inst = await web3JS.web3Init();
-        const factory = new web3Inst.eth.Contract(CampaignFactory.abi, address);
+        const factory = new web3Inst.eth.Contract(FactoryJSON.abi, address);
         return factory;
 }
 
